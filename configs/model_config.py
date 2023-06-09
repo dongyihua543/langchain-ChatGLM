b11cae9c5c15eb78e9fa9a588b39dda44e8429a0
dongyihua <dongyihua@echo.tech>
2023-06-09 17:19:41 +0800

    dev

--- configs/model_config.py
+++ configs/model_config.py
@@ -132,8 +132,8 @@ CACHED_VS_NUM = 1
 # 文本分句长度
 SENTENCE_SIZE = 100
 
-# 匹配后单段上下文长度
-CHUNK_SIZE = 250
+# 匹配后单段上下文长度 (init value: 250)
+CHUNK_SIZE = 300
 
 # LLM input history length
 LLM_HISTORY_LEN = 3
@@ -141,8 +141,8 @@ LLM_HISTORY_LEN = 3
 # return top-k text chunk from vector store
 VECTOR_SEARCH_TOP_K = 5
 
-# 知识检索内容相关度 Score, 数值范围约为0-1100，如果为0，则不生效，经测试设置为小于500时，匹配结果更精准
-VECTOR_SEARCH_SCORE_THRESHOLD = 0
+# 知识检索内容相关度 Score, 数值范围约为0-1100，如果为0，则不生效，经测试设置为小于500时，匹配结果更精准 (init value: 0)
+VECTOR_SEARCH_SCORE_THRESHOLD = 400
 
 NLTK_DATA_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), "nltk_data")
 
