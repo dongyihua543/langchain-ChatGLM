2d46a3d5945e9cb861f4c5c9f197407b056d0aac
dongyihua <dongyihua@echo.tech>
2023-06-16 10:47:42 +0800

    dev

--- models/shared.py
+++ models/shared.py
@@ -16,23 +16,23 @@ def loaderLLM(llm_model: str = None, no_remote_model: bool = False, use_ptuning_
     :param use_ptuning_v2: Use p-tuning-v2 PrefixEncoder
     :return:
     """
-    pre_model_name = loaderCheckPoint.model_name
+    pre_model_name = llm_model if llm_model else loaderCheckPoint.model_name
     llm_model_info = llm_model_dict[pre_model_name]
 
-    if no_remote_model:
-        loaderCheckPoint.no_remote_model = no_remote_model
     if use_ptuning_v2:
         loaderCheckPoint.use_ptuning_v2 = use_ptuning_v2
 
     if llm_model:
         llm_model_info = llm_model_dict[llm_model]
 
-    if loaderCheckPoint.no_remote_model:
+    if no_remote_model:
+        loaderCheckPoint.no_remote_model = no_remote_model
         loaderCheckPoint.model_name = llm_model_info['name']
+        loaderCheckPoint.model_path = llm_model_info["local_model_path"]
     else:
+        loaderCheckPoint.no_remote_model = no_remote_model
         loaderCheckPoint.model_name = llm_model_info['pretrained_model_name']
-
-    loaderCheckPoint.model_path = llm_model_info["local_model_path"]
+        loaderCheckPoint.model_path = None
 
     if 'FastChatOpenAILLM' in llm_model_info["provides"]:
         loaderCheckPoint.unload_model()
